6e0759e281552bc9afc143ae419667a91fa722a8
Tim Brinded <timbrinded@protonmail.com>
2022-02-12 09:36:09 +0000

    fixup

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -2,7 +2,6 @@ const core = require('@actions/core')
 const github = require('@actions/github')
 
 try {
-  throw new Error('Some error message')
   const name = core.getInput('who-to-greet')
 
   console.log(`Hello ${name}`)
